b4684a59fb60ba1fde7acc003df1dcba9c71ee4e
VisionMise <visionmise@kuhlonline.com>
2015-06-24 02:34:10 -0400

    Updated $ object [#97699288]

--- client/corrosionweb.js
+++ client/corrosionweb.js
@@ -24,7 +24,7 @@
 /**
  * Plugin Attributes
  */
-var title               = 'corrosion-web';
+var title               = 'corrosionweb';
 var version             = V(0,1,1);
 var author              = 'VisionMise';
 var resourceId          = 0;
@@ -53,6 +53,7 @@ var $ = function(context) {
 	this.context 	= {};
 
 	this.init 		= function(context) {
+		this.context = context;
 		return this;
 	};
 
@@ -68,7 +69,7 @@ var $ = function(context) {
         };
 
         for (var hook in this.context.hook) {
-            baseObject[hook]    = this.context.hooks[hook];
+            baseObject[hook]    = this.context.hook[hook];
         }
 
         return baseObject;
@@ -80,18 +81,62 @@ var $ = function(context) {
 		} else {
 			print(textStr);
 		}
+
+		return this;
 	};
 
 	this.broadcast 	= function() {
-
+		return this;
 	};
 
 	this.data 		= function() {
 		if (!this.context) return false;
+		return this.context.data.GetData(this.context.name);
+	};
+
+	this.save 		= function() {
+		if (!this.context) return false;
+		this.context.data.SaveData(this.context.name);
+		return this;
 	};
 
 	this.config 	= function() {
 		if (!this.context) return false;
+		return this.context.Config.Settings;
+	};
+
+	this.plugin 	= function() {
+		if (!this.context) return false;
+		return this.context.Plugin;
+	};
+
+	this.rust 		= function() {
+		if (!this.context) return false;
+		return this.context.rust;
+	};
+
+	this.consoleCommand = function(command, callback) {
+		if (!this.context) return false;
+		this.context.AddConsoleCommand(this.context.name + '.' + command, this.context.Plugin, callback);
+		return this;
+	};
+
+	this.chatCommand 	= function(command, callback) {
+		if (!this.context) return false;
+		this.context.AddChatCommand(command, this.context.Plugin, callback);
+		return this;
+	};
+
+	this.request 		= function(url, data, callback) {
+
+		var type 	= (data) ? 'POST' : 'GET';
+
+		if (type == 'GET') {
+			this.context.webrequests.EnqueueGet(url, callback.bind(this.context), this.context.plugin);
+		} else if (type == 'POST') {
+			this.context.webrequests.EnqueuePost(url + '?' + data, callback.bind(this.context), this.context.plugin);
+		}
+
 	};
 
 	return this.init(context);
@@ -113,9 +158,19 @@ var corrosionPlugin = function(pluginName, author, version, resourceId) {
 	this.hook.Init = function() {
 		self.dataObj 	= data;
 		self.rustObj 	= rust;
+
+		$(self).console("Started");
+	};
+
+	this.hook.OnServerInitialized = function() {
+		$(self).console("Initialized");
+	};
+
+	this.hook.OnPlayerInit = function(player) {
+		$(self).console(player.name + " Connected");
 	};
 
 };
 
 var plugin 			= new corrosionPlugin(title, author, version, resourceId);
-var corrosion-web 	= new $(plugin).object();
+var corrosionweb 	= $(plugin).object();
